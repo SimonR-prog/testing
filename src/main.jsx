9bd66cb608f6802fa06d8fcfd83239f15bbb3630
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-04-22 23:45:00 +0200

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import './styling/Event_card.css'
 import './styling/Calendar_days_card.css'
-import CalendarDaysCard from './CalendarDaysCard.jsx'
+
+
+// import CalendarDaysCard from './CalendarDaysCard.jsx'
+import Event_card from './components/Event_card';
+import Eventcard from './eventcard'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CalendarDaysCard />
+    {/* <CalendarDaysCard /> */}
+    <Eventcard />
+    {/* <SmallPaidCard /> */}
   </StrictMode>,
 )
+
+
